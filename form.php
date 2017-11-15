c55845a93c0e2b1e0d7a665441042bd57890e48e
swap22 <sharma.sswapnil@gmail.com>
2017-11-15 19:37:41 +0530

    name field check as empty or not

--- form.php
+++ form.php
@@ -1,10 +1,17 @@
 <?php
-
+$error_name="";
 if(isset($_POST['Submit'])){
     if(empty($_POST['Name'])){
-        echo "name is required";
+        $error_name="Name field is empty";
+    }else{
+        // post variable assign value to name or other variable
+        $name=assign($_POST['Name']);
     }
 
+}
+// function just return value it receive
+function assign($inp){
+    return $inp;
 }
 
 ?>
@@ -26,20 +33,20 @@ if(isset($_POST['Submit'])){
         			
             <fieldset><legend>* Please Fill Out the following Fields.</legend>
                 Name:<br>
-                <input class="input" type="text" Name="Name" placeholder="write your name">
+                <input class="input" type="text"  Name="Name" placeholder="write your name">
                 <br>	 
                 E-mail:<br>
-                <input class="input" type="email" Name="Email" placeholder="write your Email">
+                <input class="input" type="email"  Name="Email" placeholder="write your Email">
                 <br>
                 Gender:<br>
                 <input class="radio" type="radio" Name="Gender" value="Female">Female
                 <input class="radio" type="radio" Name="Gender" value="Male">Male
                 <br>		   
                 Website:<br>
-                <input class="input" type="text" Name="Website" placeholder="write your website address">
+                <input class="input" type="text"  Name="Website" placeholder="write your website address">
                 <br>
                 Comment:<br>
-                <textarea Name="Comment" rows="5" cols="25"></textarea>
+                <textarea Name="Comment" rows="5" cols="35"></textarea>
                 <br>
                 <br>
                 <input type="Submit" Name="Submit" value="Submit Your Information">
