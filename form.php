28e2a0d5125c6e5fd1d35298bd8f8e93d6b63a3c
swap22 <sharma.sswapnil@gmail.com>
2017-11-19 23:06:10 +0530

    displaying the message on top to fill form correctly along with error

--- form.php
+++ form.php
@@ -56,6 +56,9 @@ if(isset($_POST['Submit'])){
         echo "Website: $Website<br>";
         echo "Comments: {$_POST["Comment"]}<br>";
         }
+        else{
+            echo '<span class="Error">* Please Complete & Correct your Form & Try Again *</span>';
+        }
     }
 }
 // function just return value it receive
